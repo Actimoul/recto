16ba979df33516534638009b8dcb54e5ef07566b
Actimoul <aurelien.lecaer@gmail.com>
2026-06-28 22:02:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Recto Service Worker — v88 (cache propre + anti vieux-chunk)
-const CACHE = 'recto-v120';
+const CACHE = 'recto-v124';
 const SHELL = ['./', './index.html', './config.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (e) => {
